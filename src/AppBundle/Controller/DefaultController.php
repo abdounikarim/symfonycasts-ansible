3e44f8e22b378bc8849aa8203bc0712cf68f7391
Karim <abdounikarim@gmail.com>
2018-08-16 09:56:54 +0200

    Hello Ansible

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Video;
+use Predis\Connection\ConnectionException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        $videos = $this->getVideoRepository()
+            ->findAll();
+        $tags = $this->getUniqueOrderedTags($videos);
+
+        // Redis cache
+        try {
+            if ($this->getRedisClient()->exists('total_video_uploads_count')) {
+                $totalVideoUploadsCount = $this->getRedisClient()->get('total_video_uploads_count');
+            } else {
+                $totalVideoUploadsCount = $this->countTotalVideoUploads();
+                $this->getRedisClient()->set('total_video_uploads_count', $totalVideoUploadsCount, 'ex', 60); // 60s
+            }
+
+            if ($this->getRedisClient()->exists('total_video_views_count')) {
+                $totalVideoViewsCount = $this->getRedisClient()->get('total_video_views_count');
+            } else {
+                $totalVideoViewsCount = $this->countTotalVideoViews();
+                $this->getRedisClient()->set('total_video_views_count', $totalVideoViewsCount, 'ex', 60); // 60s
+            }
+        } catch (ConnectionException $e) {
+            $totalVideoUploadsCount = $this->countTotalVideoUploads();
+            $totalVideoViewsCount = $this->countTotalVideoViews();
+        }
+
+        return $this->render('default/index.html.twig', [
+            'videos' => $videos,
+            'tags' => $tags,
+            'totalVideoUploadsCount' => $totalVideoUploadsCount,
+            'totalVideoViewsCount' => $totalVideoViewsCount,
+        ]);
+    }
+
+    /**
+     * @param Video[] $videos
+     *
+     * @return array
+     */
+    private function getUniqueOrderedTags(array $videos)
+    {
+        $tags = [];
+
+        foreach ($videos as $video) {
+            foreach ($video->getTags() as $tag) {
+                if (!in_array($tag, $tags)) {
+                    $tags[] = $tag;
+                }
+            }
+        }
+
+        sort($tags);
+
+        return $tags;
+    }
+
+    /**
+     * @return int
+     */
+    private function countTotalVideoUploads()
+    {
+        sleep(1); // simulating a long computation: waiting for 1s
+
+        $fakedCount = intval(date('Hms') . rand(1, 9));
+
+        return $fakedCount;
+    }
+
+    /**
+     * @return int
+     */
+    private function countTotalVideoViews()
+    {
+        sleep(1); // simulating a long computation: waiting for 1s
+
+        $fakedCount = intval(date('Hms') . rand(1, 9)) * 111;
+
+        return $fakedCount;
+    }
+
+    /**
+     * @return \Doctrine\Common\Persistence\ObjectRepository
+     */
+    private function getVideoRepository()
+    {
+        return $this->get('doctrine')->getRepository(Video::class);
+    }
+
+    /**
+     * @return object|\Predis\Client
+     */
+    private function getRedisClient()
+    {
+        return $this->get('snc_redis.default_client');
+    }
+}
---
+++ src/AppBundle/Entity/Video.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="video")
+ */
+class Video
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(type="simple_array")
+     */
+    private $tags;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param string $image
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTags()
+    {
+        return $this->tags;
+    }
+
+    /**
+     * @param array $tags
+     */
+    public function setTags(array $tags)
+    {
+        $this->tags = $tags;
+    }
+}
